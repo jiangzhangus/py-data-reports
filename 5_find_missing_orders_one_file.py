a77a82c639a32227769e35557945b070671ad24e
Jiang Zhang <jzhang@humanlongevity.com>
2023-09-27 14:39:36 -0700

    fix again for the spelling

--- 5_find_missing_orders_one_file.py
+++ 5_find_missing_orders_one_file.py
@@ -19,7 +19,7 @@ finished_line_num = 0
 
 
 # Update with valid token
-prod_token = "cbd7d53b-91ec-7a34-ca6d-2d66870f4e1b"
+prod_token = "56d831ce-42b1-d8b2-e86a-8e51ead43283"
 # ----------------------------------------------------
 
 # Update to your local path for these files
@@ -260,7 +260,7 @@ def create_one_result(hli_subject_id, subject_info: dict, order_reference: dict,
         "email": subject_info.get("email", ""),
         "first_name": subject_info.get("first_name", ""),
         "last_name": subject_info.get("last_name", ""),
-        "latest_order_date": subject_info.get("lastest_order_date", ""),
+        "latest_order_date": subject_info.get("latest_order_date", ""),
         "sex": subject_info.get("sex"),
 
         "dataset_id": dataset_id,
